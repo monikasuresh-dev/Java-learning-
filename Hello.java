d08f1e67b3955e0b4636f6afa743b2a4aa39a384
IT Students <monikasuresh57@gmail.com>
2026-07-09 20:30:04 +0530

    Hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+    public static void main(String[] args){
+        int num1=5;
+        int num2=5;
+        int result = num1+num2;
+        System.out.println(result);
+    }
+    
+}
