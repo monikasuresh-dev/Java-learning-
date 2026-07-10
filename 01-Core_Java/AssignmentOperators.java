87d3873b8035897cf50d9d914ec795189e59ff56
IT Students <monikasuresh57@gmail.com>
2026-07-10 15:14:25 +0530

    AssignmentOperators

---
+++ 01-Core_Java/AssignmentOperators.java
@@ -0,0 +1,21 @@
+public class AssignmentOperators {
+    public static void main(String[] args){
+        int num1 = 2;
+        int num2 = 4;
+        //num1++;
+        //++num1;
+        //--num2;
+        //num2--;
+         int sum=num1+num2;
+         int diff=num1-num2;
+         int product=num1*num2;
+         int division=num1/num2;
+         int remainder=num1%num2;
+         System.out.println("Sum= "+sum);
+         System.out.println("DIIFERENCE="+diff);
+            System.out.println("PRODUCT="+product);
+            System.out.println("DIVISION="+division);
+            System.out.println("REMAINDER="+remainder);
+    }
+    
+}
