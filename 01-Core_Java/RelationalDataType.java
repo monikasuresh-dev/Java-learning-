aa5cceda9153187e55940d0be5fb72c677b147b9
IT Students <monikasuresh57@gmail.com>
2026-07-13 21:56:07 +0530

    RelationalDataType

---
+++ 01-Core_Java/RelationalDataType.java
@@ -0,0 +1,9 @@
+public class RelationalDataType {
+    public static void main(String[] args){
+        int x=90;
+       int y=90;
+        boolean result=x<=y;
+        System.out.println(result);
+
+    }
+}
